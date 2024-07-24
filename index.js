f1ab046fb53c9ef79e99f78f482b6de5f96ea465
Oltiariq3561 <oltiariq356@gmail.com>
2024-07-23 17:26:36 -0700

    tamom

---
+++ index.js
@@ -0,0 +1,130 @@
+// let yigindi = 0;
+// for(let i = 9; i<=100; i+=2){
+// yigindi=yigindi+i
+// }
+// console.log(yigindi);
+
+// 2-masala
+// let a = +prompt('son');
+// let faktorial = 1;
+// for(let i = 1; i <= a; i++){
+// faktorial*=i
+// }
+// console.log(faktorial);
+
+// 4-masala
+// let a = 20;
+// let kvadrat = 1;
+// let  yigindi = 0;
+// for(let i = 1; i<=a; i++){
+// kvadrat=kvadrat+i**2
+// }
+// for(let b = 1; b<=kvadrat; b++){
+// yigindi=yigindi+b
+// }
+// console.log(yigindi);
+
+// 7-masala
+// let raqam = prompt("Raqam kiriting:");
+// let teskari= '';
+// for (let i = raqam.length - 1; i >= 0; i--) {
+//     teskari += raqam[i];
+// }
+// console.log(teskari);
+
+// 9-masala
+// for(let i = 1; i<=50; i ++){
+// if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(i);
+// }
+// }
+
+// 10-masala
+// let sum = 0;
+// let count = 0;
+// for (let i = 1; i<=100; i++){
+// sum+=i;
+// count++
+// }
+// let res = sum/count;
+// console.log(res);
+
+// 11-masala
+// let a = prompt('son');
+// let length = a.length;
+// console.log(length);
+
+// 12-masala
+// let a = +prompt('son');
+// let manfiy = 0;
+// manfiy = manfiy - a;
+// console.log(manfiy); 
+
+// 14-masala
+// let res=0;
+// for(let i = 0; i<=200; i+=2){
+// res=res+i
+// }
+// console.log(res);
+
+// 15-masala
+// let res=0;
+// for(let i = 0; i<=20; i++){
+// let sum = i+(i+1);
+// res+=sum;
+// }
+// console.log(res);
+
+
+
+
+
+
+// 1 masala
+// for(let i = -1; i <= 10; i++){
+//     if(i == 5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// 2 masala
+
+// for(let i = -1; i <= 10; i++){
+//     if(i == 3 || i == 7){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// 3 masala
+
+// let res = "";
+// for(let i = 1; i <= 1; i++){
+//     let a = +prompt("a");
+//     if(a == 7){
+//         break;
+//         res = "topildi";
+//     } else {
+//         res = "topilmadi";
+//     }
+//     console.log(res);
+// }
+
+// 4 masala
+
+// for(let i = -1; i <= 10; i++){
+//     if(i % 2 == 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// 5 masala
+
+// for(let i = -1; i <= 100; i++){
+//     if(i > 50){
+//         break;
+//     }
+//     console.log(i);
+// }
